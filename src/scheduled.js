3683fd0fc7c742b9ffbb5a35fb57c2e10fb68fb8
Mark Rawls <markrawls96@gmail.com>
2020-03-29 19:33:59 -0400

    Delete old messages when the person goes offline

--- src/scheduled.js
+++ src/scheduled.js
@@ -14,7 +14,7 @@ export const notifyNewStreams = client => async () => {
   }
 
   if (!fs.existsSync(FILE_NAME)) {
-    fs.writeFileSync(FILE_NAME, JSON.stringify({ knownChannels: [] }));
+    fs.writeFileSync(FILE_NAME, JSON.stringify({ knownChannels: {} }));
   }
 
   // Get the list of known channels currently
@@ -25,30 +25,58 @@ export const notifyNewStreams = client => async () => {
 
   // Get the union data
   const notifyAbout = currentlyLiveStreams.filter(
-    stream => !fileData.knownChannels.includes(stream.user_name),
+    stream => !fileData.knownChannels.hasOwnProperty(stream.user_name),
   );
 
-  // Write the new data back to the file
-  fs.writeFileSync(
-    FILE_NAME,
-    JSON.stringify({ knownChannels: currentlyLiveStreams.map(s => s.user_name) }),
+  // Get the inverse of that union
+  const deleteMessageIds = Object.entries(fileData.knownChannels).reduce(
+    (acc, [userName, messageId]) => {
+      const liveUserNames = notifyAbout.map(s => s.user_name);
+
+      if (!liveUserNames.includes(userName)) {
+        return acc.concat([messageId]);
+      }
+
+      return acc;
+    },
+    [],
   );
 
   const newsChannel = await client.channels.fetch(channels.NEWS);
+  const knownChannels = {};
+
+  try {
+    const messagesToDelete = await newsChannel.awaitMessages(m => deleteMessageIds.includes(m.id));
+
+    for (const m of messagesToDelete) {
+      await m.delete();
+    }
+  } catch {
+    console.log('Error deleting messages');
+  }
 
   for (const stream of notifyAbout) {
-    newsChannel.send(
-      new MessageEmbed()
-        .setColor(EMBED_COLOR)
-        .setTitle(stream.user_name)
-        .setURL(`https://twitch.tv/${stream.user_name}`)
-        .setThumbnail(stream.thumbnail_url.replace('{width}', '72').replace('{height}', '72'))
-        .setDescription(stream.title)
-        .addFields({
-          name: 'REMEMBER:',
-          value: snark[Math.floor(Math.random() * snark.length)],
-        })
-        .setTimestamp(),
-    );
+    try {
+      const message = await newsChannel.send(
+        new MessageEmbed()
+          .setColor(EMBED_COLOR)
+          .setTitle(stream.user_name)
+          .setURL(`https://twitch.tv/${stream.user_name}`)
+          .setThumbnail(stream.thumbnail_url.replace('{width}', '72').replace('{height}', '72'))
+          .setDescription(stream.title)
+          .addFields({
+            name: 'REMEMBER:',
+            value: snark[Math.floor(Math.random() * snark.length)],
+          })
+          .setTimestamp(),
+      );
+
+      knownChannels[stream.user_name] = message.id;
+    } catch {
+      console.log(`Error notifying about ${stream.user_name}`);
+    }
   }
+
+  // Write the new data back to the file
+  fs.writeFileSync(FILE_NAME, JSON.stringify({ knownChannels }));
 };
