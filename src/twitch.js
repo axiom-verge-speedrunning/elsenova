8c533ec1746f5ba1f051ea1c980521abbacd580d
Mark Rawls <markrawls96@gmail.com>
2020-05-13 17:03:59 -0400

    Get twitch auth token

--- src/twitch.js
+++ src/twitch.js
@@ -7,12 +7,29 @@ dotenvConfig();
 const HEADERS = { 'Client-ID': process.env.TWITCH_CLIENT_ID };
 
 class TwitchAPI {
+  static async getToken() {
+    return axios.post(
+      'https://id.twitch.tv/oauth2/token',
+      {},
+      {
+        params: {
+          client_id: process.env.TWITCH_CLIENT_ID,
+          client_secret: process.env.TWITCH_CLIENT_SECRET,
+          grant_type: 'client_credentials',
+        },
+      },
+    );
+  }
+
   static async getStreams() {
+    const response = await this.getToken();
+    const authToken = response.data.access_token;
+
     return axios.get('https://api.twitch.tv/helix/streams', {
       params: {
         game_id: TWITCH_GAME_ID,
       },
-      headers: HEADERS,
+      headers: { ...HEADERS, Authorization: `Bearer ${authToken}` },
     });
   }
 }
