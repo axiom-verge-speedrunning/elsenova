481df9e82dfefd55f3a63b974a605163919df177
Mark Rawls <markrawls96@gmail.com>
2021-04-18 01:15:21 -0400

    Whoops

--- src/constants.js
+++ src/constants.js
@@ -2,5 +2,6 @@ import 'environment';
 
 export const TWITCH_GAME_ID = process.env.TWITCH_GAME_ID || '34072';
 export const NEWS_CHANNEL_ID = process.env.NEWS_CHANNEL_ID || '774801012052983829';
+export const RANDO_NEWS_CHANNEL_ID = process.env.RANDO_NEWS_CHANNEL_ID;
 export const MOD_ROLE_ID = process.env.MOD_ROLE_ID || '675583185547034665';
 export const EMBED_COLOR = '#0099ff';
